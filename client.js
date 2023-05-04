f0a8c94063b80634a36a4f8331f6691cbb71e501
Tyler Burns <Tburnsie10@gmail.com>
2023-05-03 21:50:33 -0400

    finished

--- client.js
+++ client.js
@@ -11,7 +11,17 @@ console.log('Step 0 complete! client.js loaded!');
 
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
 
+
+
+let highScore = 29;
+
+
+
 // 2. Make a new empty array called myScrabbleTiles.
+
+
+let myScrabbleTiles = [];
+
 
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
@@ -23,7 +33,28 @@ console.log('Step 0 complete! client.js loaded!');
   { tile: 'A', score: 1 }
 */
 
+let array = [
+  { tile: 'N', score: 1 },
+  { tile: 'K', score: 5 },
+  { tile: 'Z', score: 10 },
+  { tile: 'X', score: 8 },
+  { tile: 'D', score: 2 },
+  { tile: 'A', score: 1 },
+]
+
+for(let items of array) {
+  myScrabbleTiles.push(items)
+
+}
+console.log(myScrabbleTiles)
+
+
 // 4. Remove the last tile from myScrabbleTiles.
+//pop method
+
+myScrabbleTiles.pop();
+
+console.log(myScrabbleTiles)
 
 
 // 5. Add the following new tile to myScrabbleTiles:
@@ -31,17 +62,49 @@ console.log('Step 0 complete! client.js loaded!');
   { tile: 'F', score : 4 }
 */
 
+myScrabbleTiles.push({ tile: 'F', score : 4 })
+
+console.log(myScrabbleTiles);
+
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
 function sumTiles(anArray) {
-
+  let sum = 0;
+  for(let i = 0; i <anArray.length; i++) {
+    sum += anArray[i]['score']
+  }
+  return sum;
 }
+  
+
+  
+  
 
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
 
+let myScore = sumTiles(myScrabbleTiles);
+console.log(myScore);
+
 
 // 8. Check whether or not your score is higher than the highScore.
+
+myScore > highScore 
+
+console.log(myScore > highScore)
+
 // 8-1 If your score is higher, change highScore to the new high score.
 
+console.log(highScore)
+
+
+if(myScore > highScore) {
+  highScore = myScore
+  console.log('High Score!')
+} else {
+  console.log('Nice Try!')
+}
+
+
+console.log(highScore)
 
 // DONE!
